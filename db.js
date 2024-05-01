eebb2a3fcde3b49074b26346f37759fe1d033cd0
emil-98 <ewelton98@gmail.com>
2024-05-01 11:04:01 -0600

    Update files

--- db.js
+++ db.js
@@ -4,7 +4,12 @@ import pg from 'pg'
 const {Pool} = pg
 
 const pool = new Pool({
-  host: process.env.DB_HOST
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  ssl: false
 })
 
 export default pool
